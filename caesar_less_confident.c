30484403fca0ff1b7f05068a4dd78e27349ccbd0
datarec <73265375+datarec@users.noreply.github.com>
2025-06-06 13:37:18 +1000

    Update caesar_less_confident.c
    passed all testing, she works!

--- caesar_less_confident.c
+++ caesar_less_confident.c
@@ -7,42 +7,39 @@
 int cypher_text(char* argv[]) 
 {
 	int key = atoi(argv[1]);
-	char letters[26] = "abcdefghijklmnopqrstuvwxyz";
 	char plaintext[100];
 	printf("plaintext: ");
 	fgets(plaintext, 100, stdin);
 	printf("cyphertext: ");
 	for (int i = 0; plaintext[i] != '\0'; i++) 
 	{
-		if (plaintext[i] + key > 'z') 
+		if (plaintext[i] + key > 'z')
 		{
-			printf("%c", plaintext[i] = 'z' - plaintext[i]);
-			printf("%c", 'a' + plaintext[i]);
+			printf("%c", (plaintext[i] + key - 'z') + 'a' - 1);
 		}
-		else if (plaintext[i] >= 'a' || plaintext[i] <= 'z')
-		{
-			printf("%c", plaintext[i] + key);
-		}
-		else
+		else if (plaintext[i] < 'A' || plaintext[i] < 'a')  
 		{
 			printf("%c", plaintext[i]);
 		}
+		else 
+		{
+			printf("%c", plaintext[i] + key);
+		}
 	}
 	printf("\n");
-
 }
 
 int main(int argc, char* argv[]) 
 {
 	char errormsg[20] = "Usage: ./caesar key";
+	if (argc != 2) 
+	{
+		printf("%s\n", errormsg);
+		return 1;
+	}	
 	for (int i = 0; argv[1][i] != '\0'; i++) 
 	{
-		if (argc != 2) 
-		{
-			printf("%s\n", errormsg);
-			return 1;
-		}
-		else if (argv[1][i] >= 'a' && argv[1][i] <= 'z') 
+		if (argv[1][i] >= 'a' && argv[1][i] <= 'z') 
 		{
 			printf("%s\n",  errormsg);
 			return 1;
