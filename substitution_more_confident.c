4ad405d3b524981acea8de4126a16c55f6d52adf
datarec <73265375+datarec@users.noreply.github.com>
2025-06-08 11:51:32 +1000

    Update substitution_more_confident.c
    small patch

--- substitution_more_confident.c
+++ substitution_more_confident.c
@@ -12,19 +12,27 @@ int main(int argc, char* argv[])
 	}
 	int length = retLen(argv);
 	int isAlpha = checkAlpha(argv);
-	if (length != 26 && isAlpha == 0) 
+	if (length != 26 || isAlpha == 1) 
 	{
-		printf("Key must be alphabetical and 26 letters in length.\n");
+		printf("Usage: ./substitution key (MAX-MIN 26 letters)\n");
 		return 1;
 	}
 }
 
 int checkAlpha(char* argv[]) 
-{
-	for (int i = argv[1][i] != '\0'; i++) 
+{ 
+	for (int i = 0; argv[1][i] != '\0'; i++) 
 	{
-		
+		if (argv[1][i] < 'A' || argv[1][i] > 'Z' && argv[1][i] < 'a') 
+		{
+			return 1;
+		}
+		else if (argv[1][i] > 'z') 
+		{
+			return 1;
+		}
 	}
+	return 0;
 }
 
 int retLen(char* argv[]) 
